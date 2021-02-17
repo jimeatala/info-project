2c9ae3ccb26b873830a8a710133c760cacd902be
Nicole Nobbay <nmnobbs@uw.edu>
2021-02-17 01:20:01 -0800

    more additions

--- group-project.R
+++ group-project.R
@@ -31,3 +31,36 @@ for(i in seq_along(dataset2)) {
 ahhh <- likert(dataset2)
 
 
+# Bar chart for gender and mental health ratings
+
+ggplot(data = dataset2) +
+  geom_col(mapping = aes(x = `how would you rate your health`, 
+      y = gender, fill = gender)) +
+  labs(title = "Mental Health Rating by Gender", x = "Level",
+       y = "Gender") +
+  scale_fill_brewer(palette = "Paired")
+
+# Separated by Gender- how many responses in each "Rate of Health" level
+
+colors <- c(rep("red",1), rep("orange",1), rep("yellow",1), rep("green",1), rep("blue",1),
+            rep("black",2), rep("red",1), rep("orange",1), rep("yellow",1), rep("green",1),
+            rep("blue",1), rep("black",3))
+
+ggplot(data = dataset2, mapping = aes(x = `how would you rate your health`)) + 
+  geom_histogram(binwidth = 1, stat = "count",
+                 color = "white", fill = colors) +
+  labs(x = "Rate of Health") +
+  facet_wrap(~gender, labeller = "label_both") 
+
+# Stacked Bar Chart x Comparison of 4 Questions
+
+data_items <- dataset2 %>% 
+  select(`talk with neighbor in month`:`how often did you stay in touch with family/friend before covid`) %>%
+  na.omit 
+
+data_items %>%  
+  gather(key = Questions, value = Responses) %>% 
+  mutate(Responses = factor(Responses)) %>% 
+  ggplot(aes(x = Questions)) +
+  geom_bar(aes(fill = Responses), position = "fill") -> interactions
+interactions + scale_fill_brewer(palette = "Spectral")
